778c1542a7551ee60bf481b5af50e9c45a71687c
孙士雄 <sunshix@seeyon.com>
2024-09-30 18:21:00 +0800

    新建项目

---
+++ eleganteer-business-system/eleganteer-upms/src/main/java/com/eleganteer/eleganteerupms/EleganteerUpmsApplication.java
@@ -0,0 +1,13 @@
+package com.eleganteer.eleganteerupms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleganteerUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleganteerUpmsApplication.class, args);
+    }
+
+}
---
+++ eleganteer-business-system/eleganteer-upms/src/test/java/com/eleganteer/eleganteerupms/EleganteerUpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleganteer.eleganteerupms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleganteerUpmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
