0246eab4d9b2054c6ba61fa832845e97da268088
孙士雄 <sunshix@seeyon.com>
2024-10-31 18:31:23 +0800

    refactor(gradle): 重构 Gradle 构建配置并调整项目结构

---
+++ eleganteer-system/eleganteer-system-biz/src/main/java/com/eleganteer/systembiz/EleganteerSystemBizApplication.java
@@ -0,0 +1,13 @@
+package com.eleganteer.systembiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleganteerSystemBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleganteerSystemBizApplication.class, args);
+    }
+
+}
---
+++ eleganteer-system/eleganteer-system-facad/src/main/java/com/eleganteer/systemfacad/EleganteerSystemFacadApplication.java
@@ -0,0 +1,13 @@
+package com.eleganteer.systemfacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleganteerSystemFacadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleganteerSystemFacadApplication.class, args);
+    }
+
+}
