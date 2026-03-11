ae93a8676f85d77ca36a6abdec48f5f74d12323e
sunshixiong <1285913468@qq.com>
2026-03-11 22:34:06 +0800

    refactor(core): 重构项目结构及配置文件
    - 将core、gateway、system模块配置文件迁移并重命名为server模块对应的application.yml
    - 调整spring.application.name以匹配新模块名称
    - core模块主启动类及测试类重构为authorization-server模块，并更新包名及类名
    - 升级build.gradle中Spring Boot版本至4.0.3及调整Java版本至25
    - 为多个starter模块新增build.gradle和settings.gradle文件
    - 删除system

---
+++ eleganteer-base-server/eleganteer-authorization-server/src/main/java/com/eleganteer/authorization/EleganteerAuthorizationApplication.java
@@ -0,0 +1,16 @@
+package com.eleganteer.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sunshixiong
+ */
+@SpringBootApplication
+public class EleganteerAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleganteerAuthorizationApplication.class, args);
+    }
+
+}
---
+++ eleganteer-base-server/eleganteer-authorization-server/src/test/java/com/eleganteer/authorization/EleganteerAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleganteer.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleganteerAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
